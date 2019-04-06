2c1c0211151fe7d45c29ffd272abc89b259e9790
Jal Panchal <jal08panchal@gmail.com>
2019-04-06 05:32:04 -0700

    Adding publicDir path with middleware

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+const publicDir = path.join(__dirname,"/public")
+
+
+app.use(express.static(publicDir))
+
+app.listen(port, (req,res) => {
+    console.log("Server is listening!!")
+})
