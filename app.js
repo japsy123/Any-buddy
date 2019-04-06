6fc02be6b5f20c3b99b44877d965a95818b3a10b
Jal Panchal <jal08panchal@gmail.com>
2019-04-06 06:01:25 -0700

    Configuring socket.io

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+const scoketio = require("socket.io");
+const http = require("http");
 
-const publicDir = path.join(__dirname,"/public")
-
+const server = http.createServer(app);
+const publicDir = path.join(__dirname,"/public");
+const io = scoketio(server);
 
 app.use(express.static(publicDir))
 
-app.listen(port, (req,res) => {
+server.listen(port, (req,res) => {
     console.log("Server is listening!!")
 })
