479bafe508d3f660a83244cf8c05bf192822d3f9
Jal Panchal <jal08panchal@gmail.com>
2019-04-06 13:23:31 -0700

    Adding geolocation event for chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,27 @@ document.querySelector("#sendMessage").addEventListener("click", function(e){
     socket.emit("sendMessage", message)
 })
 
+document.querySelector("#myLocation").addEventListener("click", function(e){
+
+    e.preventDefault();
+    if(!navigator.geolocation){
+        return alert("Geolocation isnt supported for your browser");
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+
+        socket.emit("sendLocations", {
+            lat,
+            long
+        })
+    })
+})
+
+
+
 socket.on("message", (message)=>{
     console.log(message)
 
